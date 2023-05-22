265ae91329c8bb24eef361231a630d3b4d7bf3ed
Carol0161 <78099991+Carol0161@users.noreply.github.com>
2023-05-22 10:42:27 +0200

    Update HelloWorld.java

--- src/gitprueba/HelloWorld.java
+++ src/gitprueba/HelloWorld.java
@@ -3,7 +3,7 @@ package gitprueba;
 public class HelloWorld {
 	
 	public static void main(String[] args) {
-		System.out.println("hello world GIY");
+		System.out.println("hello world GIY desde git");
 	}
 	
 
